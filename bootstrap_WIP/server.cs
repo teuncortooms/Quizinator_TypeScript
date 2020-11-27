b23f4be0b71f800ed8db77c38e13a809b2219c9f
Teun Cortooms <teuncortooms@gmail.com>
2020-11-27 21:57:42 +0100

    bootstrap?

---
+++ bootstrap_WIP/server.cs
@@ -0,0 +1,11 @@
+String connectionInfo = ""
+    + "server=mssql.fhict.local;"
+    + "database=dbi884573;"
+    + "user id=dbi884573;"
+    + "password=quiz123;"
+    + "connect timeout=30;";
+
+// Server=mssql.fhict.local;
+// Database=dbi884573;
+// User Id = dbi884573;
+// Password=quiz123;
